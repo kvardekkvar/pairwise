0d16735b8a4d5ff699f3831e07e7b4920d612847
liaombro <85120539+liaombro@users.noreply.github.com>
2021-05-31 19:06:00 +0300

    Update pairwisebinary.py

--- pairwisebinary.py
+++ pairwisebinary.py
@@ -2,29 +2,27 @@ import itertools
 import numpy as np
 import scipy.special
 
-k=1
-cnt=0
+k = 1
+cnt = 0
 A = []
 
-n=int(input("Число столбцов:"))
+n = int(input("Число столбцов (должно быть >=6):"))
+while scipy.special.binom(2*k, k) < 2*n:
+    k = k + 1
 
-while(scipy.special.binom(2*k, k)<2*n):
-    k=k+1
+res = np.zeros((2*k, n), dtype=int)
 
 for bits in itertools.combinations(range(2*k), k):
     if 0 not in bits:
         continue
-    s = ['0'] * 2*k
     for bit in bits:
-        s[bit] = '1'
-    A.append(''.join(s))
-    cnt=cnt+1
-    if cnt>=n:
+        res[bit, cnt] = 1
+    cnt = cnt+1
+    if cnt >= n:
         break
 
-res=np.empty((0,2*k), dtype=int)
-for a in A:
-    res=np.vstack((res, list(a)))
-res=res.transpose()
-for s in res:
-    print("".join(s))
+
+for i in res:
+    for j in i:
+        print(j, end='')
+    print()
