c0852a7e7341df369a8c47a0b5fc24662fc6be3c
liaombro <85120539+liaombro@users.noreply.github.com>
2021-05-31 17:47:39 +0300

    Add files via upload

---
+++ pairwisebinary.py
@@ -0,0 +1,30 @@
+import itertools
+import numpy as np
+import scipy.special
+
+k=1
+cnt=0
+A = []
+
+n=int(input("Число столбцов:"))
+
+while(scipy.special.binom(2*k, k)<2*n):
+    k=k+1
+
+
+
+for bits in itertools.combinations(range(2*k), k):
+    s = ['0'] * 2*k
+    for bit in bits:
+        s[bit] = '1'
+    A.append(''.join(s))
+    cnt=cnt+1
+    if cnt>=n:
+        break
+
+res=np.empty((0,2*k), dtype=int)
+for a in A:
+    res=np.vstack((res, list(a)))
+res=res.transpose()
+for s in res:
+    print("".join(s))
